e204d0096943fd23b807f0af73ae238f7c51ba6e
jjmcinto <jjmcinto@andrew.cmu.edu>
2021-06-20 14:19:17 -0700

    tidied up the comments

--- roboNanny.py
+++ roboNanny.py
@@ -23,11 +23,8 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='/Users/vdo/Documents/Jeffrey/CMU/C
 expectedFaceCount = int(input("Enter expected count of attendees: "));
 
 browser = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver/geckodriver")
-#browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
-#browser = webdriver.Firefox();
 browser.get("https://www.zoom.us/");
 input("Press RETURN when everyone has arrived.");
-#sleep(30);
 countUses = 0;
 try:
     while browser:
@@ -46,7 +43,7 @@ try:
             else:
                 os.system('say "Alert! ' + str(expectedFaceCount - faceCount) + ' attendees are missing!"');
         
-        #wait 30 seconds before checking again
+        #wait a few seconds before checking again
         sleep(10);
 except Exception as e:
     print('Google Vision Calls: ' + str(countUses));
